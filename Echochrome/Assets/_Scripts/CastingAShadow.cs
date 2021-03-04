b4549644e0a63295d4731c783e150e7c290370ed
Valentin <kibirevvalik202@gmail.com>
2021-03-04 11:42:04 +0300

    [update] shadow

--- Echochrome/Assets/_Scripts/CastingAShadow.cs
+++ Echochrome/Assets/_Scripts/CastingAShadow.cs
@@ -7,21 +7,43 @@ public class CastingAShadow : MonoBehaviour
     [SerializeField]
     private Transform _lightbulb;
     [SerializeField]
+    private MeshFilter _mesh;
+    [SerializeField]
     private Shadow _shadow;
 
     void Start()
     {
-
+        Shading();
     }
 
     void FixedUpdate()
     {
-        Ray ray = new Ray(_lightbulb.position, transform.position) ;
-
-        Vector3 direction = (transform.position - _lightbulb.position);
-
-        _shadow.transform.position = _lightbulb.position + ((direction) *
-                        ((5.68f) / direction.z));
-        _shadow.SetRotaionShpdow(_lightbulb.position);
+        Shading();
+        //_shadow.SetRotaionShpdow(_lightbulb.position);
     }
+    private void Shading()
+    {
+        Vector3[] vertices = GetMeshVerticesGlobal();
+        Vector3[] newVertices = new Vector3[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 direction = (vertices[i] - _lightbulb.position);
+
+            newVertices[i] = _lightbulb.position + ((direction) *
+                            ((5.68f) / direction.z));
+            newVertices[i] = _shadow.transform.InverseTransformPoint(newVertices[i]);
+        }
+        _shadow.SetMeshVertices(newVertices);
+    }
+    private Vector3[] GetMeshVerticesGlobal()
+    {
+        Vector3[] vertices = _mesh.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = transform.TransformPoint(vertices[i]);
+        }
+        return vertices;
+    }
+
 }
--- Echochrome/Assets/_Scripts/Shadow.cs
+++ Echochrome/Assets/_Scripts/Shadow.cs
@@ -6,6 +6,17 @@ public class Shadow : MonoBehaviour
 {
     [SerializeField]
     private Transform _shodow;
+    [SerializeField]
+    private MeshFilter _mesh;
+    private void Start()
+    {
+        //Vector3[] Vertex = _mesh.mesh.vertices;
+        ////for (int i = 0; i < Vertex.Length; i++)
+        ////{
+        ////    Vertex[i].z = 0;
+        ////}
+        //_mesh.mesh.vertices = Vertex;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -14,5 +25,18 @@ public class Shadow : MonoBehaviour
     public void SetRotaionShpdow(Vector3 Position)
     {
         _shodow.LookAt(Position);
+    }
+    public Vector3[] GetMeshVerticesGlobal()
+    {
+        Vector3[] vertices = _mesh.mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = transform.TransformPoint(vertices[i]);
+        }
+        return vertices;
+    }
+    public void SetMeshVertices(Vector3[] vertices)
+    {
+        _mesh.mesh.vertices = vertices;
     }
 }
