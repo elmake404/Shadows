4519702ccac16a4e84efa012e262151dcd40c294
Valentin <kibirevvalik202@gmail.com>
2021-03-05 11:21:56 +0300

    [updte] level

---
+++ Echochrome/Assets/_Scripts/Lightbulb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightbulb : MonoBehaviour
+{
+    [SerializeField]
+    private float _speedMove;
+    //[SerializeField]
+    //private float _
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
