c69b3f251aabc786167ebc6006b51b3c7651de05
Valentin <kibirevvalik202@gmail.com>
2021-03-08 12:57:17 +0300

    [add] controller

--- Echochrome/Assets/_Scripts/Lightbulb.cs
+++ Echochrome/Assets/_Scripts/Lightbulb.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Lightbulb : MonoBehaviour
 {
+    private Vector3 _startMosePos, _currentMosePos, _lightbulbPos;
     [SerializeField]
-    private float _speedMove;
-    //[SerializeField]
-    //private float _
+    private float _speedMove,_limmitY ,_limmitX;
+    
     private void Start()
     {
         
@@ -15,6 +15,16 @@ public class Lightbulb : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _lightbulbPos = transform.position;
+            _startMosePos = Input.mousePosition/10;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            _currentMosePos = Input.mousePosition/10;
+            Vector3 pos = _lightbulbPos + (_currentMosePos - _startMosePos);
+            transform.position = Vector3.Lerp(transform.position,pos,_speedMove*Time.deltaTime);
+        }
     }
 }
